936aa4aa9be9f1f8355a5e49f52becc806c76f68
John Yani <vanuan@gmail.com>
2013-07-03 03:18:39 +0300

    Add example for arguments list

---
+++ tests/obj2.py
@@ -0,0 +1,11 @@
+from globals import ObjectStateHolder
+
+def main(F):
+    o = ObjectStateHolder('OSH')
+    if condition:
+        attrName = 'n1'
+    else:
+        attrName = 'n2'
+    o.setAttribute(attrName, 'val')
+
+main(F)
