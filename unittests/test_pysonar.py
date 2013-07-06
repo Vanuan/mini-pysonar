49e39933e070d6c7ca2772ddf0c50a9adee525c2
Vladimir Vitvitskiy <vladimir.vitvitskiy@hp.com>
2013-07-06 14:12:13 +0300

    add test for the slist function

--- unittests/test_pysonar.py
+++ unittests/test_pysonar.py
@@ -5,6 +5,21 @@ Created on Jul 5, 2013
 '''
 import unittest
 import pysonar as ps
+import lists
+from lists import Pair as P, nil
+
+class TestOperationsOnList(unittest.TestCase):
+    
+    def test_slits(self):
+        self.assertEqual(P(1, P(2, P(3, nil))), lists.slist([1, 2, 3]))
+        
+
+class TestListType(unittest.TestCase):
+    
+    def test_equality(self):
+        l1 = lists.slist([1, 2, 3])
+        l2 = lists.slist([1, 2, 3])
+        self.assertEqual(l1, l2)
 
 
 class Test(unittest.TestCase):
