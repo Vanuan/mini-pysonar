7f047d6465715adc466aa909365d52e3775660ad
Vladimir Vitvitskiy <vladimir.vitvitskiy@hp.com>
2013-07-06 13:23:35 +0300

    extend testing module with additional assert methods

--- tasty.py
+++ tasty.py
@@ -1,17 +1,57 @@
 
 from unittest.case import TestCase
 import ast
+import pysonar as ps
 import functools
+import lists
 
-class _DummyTest(TestCase):
+
+class GetNodeValue(ast.NodeVisitor):
+    
+    def visit_Num(self, node):
+        return node.n
+
+_get_value = GetNodeValue().visit
+
+class PysonarTest(TestCase):
 
     def runTest(self):
         pass
     
+    def _assert(self, expected, actual, msg="Different values"):
+        if expected != actual:
+            raise AssertionError("%s: %s != %s" % (msg, expected, actual))
+    
+    def _assertType(self, expected_type, actual_value):
+        if not isinstance(actual_value, expected_type):
+            raise AssertionError("Expected type %s but got %s" %
+                                 (expected_type, actual_value))
+
+    
+    def assertNum(self, expected, actual):
+        self._assertType(ast.Num, actual)
+        self._assert(expected, actual.n)
+    
+    def assertDict(self, expected, actual):
+        '@types: dict, pysonar.DictType'
+        self._assertType(ps.DictType, actual)
+        actual = actual.dict
+        self._assert(len(expected), lists.length(actual), "Size mismatch")
+        for item_pair in actual:
+            keys, values = item_pair.fst, item_pair.snd
+            actual_key = _get_value(keys[0])
+            actual_value = _get_value(values[0])
+            self._assert(expected.get(actual_key), actual_value,
+                         "Different values for the same key: %s" % actual_key)
+
+            
+        
+
+    
 def as_unit(fn):
     @functools.wraps(fn)
     def wrapper():
-        return fn(_DummyTest())
+        return fn(PysonarTest())
     return wrapper
 
 
